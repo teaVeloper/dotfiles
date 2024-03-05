9c6463190a1c73a8e34d750669039aef31910498
Bertold Sedlak <bertold.sedlak@cyansecurity.com>
2024-03-05 01:28:29 +0100

    add mini.nvim and clean plugins
    remove plugins i dont like or use
    add mini.nvim
    replace lualine with mini.nvim statusline

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -71,27 +71,4 @@ return {
             end,
         },
     },
-
-    {
-        -- Set lualine as statusline
-        "nvim-lualine/lualine.nvim",
-        -- See `:help lualine.txt`
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = "tokyonight",
-                component_separators = "|",
-                section_separators = "",
-            },
-        },
-    },
-
-    {
-        -- Add indentation guides even on blank lines
-        "lukas-reineke/indent-blankline.nvim",
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help indent_blankline.txt`
-        main = "ibl",
-        opts = {},
-    },
 }
